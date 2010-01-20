93b7571dbb68f647acdf8ea2812fecaf1a1e26e9
idojun <idojun@18163362-01b4-11df-b4af-25c34a55dae2>
2010-01-20 08:33:18 +0000

    change filename modify comment of affine*.[c,cpp]

---
+++ trunk/c/affine1.c
@@ -0,0 +1,46 @@
+#include "cv.h"
+#include "highgui.h"
+
+int
+main(int argc, char **argv)
+{
+  IplImage *src_img = 0, *dst_img = 0;
+  CvMat *affine_matrix;
+  CvPoint2D32f src_pnt[3], dst_pnt[3];
+
+  // (1)画像の読み込み（カラー），出力用画像領域の確保を行ないます．
+  const char *imagename = argc > 1 ? argv[1] : "fruit.png";
+  src_img = cvLoadImage(imagename, CV_LOAD_IMAGE_COLOR);
+  if(src_img == 0) return -1;
+  dst_img = cvCloneImage(src_img);
+
+  // (2)三角形の回転前と回転後の対応する頂点をそれぞれセットし
+  //    cvGetAffineTransform を用いてアフィン行列を求めます．
+  src_pnt[0] = cvPoint2D32f(200.0, 200.0);
+  src_pnt[1] = cvPoint2D32f(250.0, 200.0);
+  src_pnt[2] = cvPoint2D32f(200.0, 100.0);
+  dst_pnt[0] = cvPoint2D32f(300.0, 100.0);
+  dst_pnt[1] = cvPoint2D32f(300.0, 50.0);
+  dst_pnt[2] = cvPoint2D32f(200.0, 100.0);
+  affine_matrix = cvCreateMat(2, 3, CV_32FC1);
+  cvGetAffineTransform(src_pnt, dst_pnt, affine_matrix);
+
+  // (3)求めたアフィン行列を用いて，cvWarpAffineを用いて画像を回転させます．
+  cvWarpAffine(src_img, dst_img, map_matrix, CV_INTER_LINEAR + CV_WARP_FILL_OUTLIERS, cvScalarAll(0));
+
+
+  // (4)結果を表示します．
+  cvNamedWindow("src", CV_WINDOW_AUTOSIZE);
+  cvNamedWindow("dst", CV_WINDOW_AUTOSIZE);
+  cvShowImage("src", src_img);
+  cvShowImage("dst", dst_img);
+  cvWaitKey(0);
+
+  cvDestroyWindow("src");
+  cvDestroyWindow("dst");
+  cvReleaseImage(&src_img);
+  cvReleaseImage(&dst_img);
+  cvReleaseMat(&map_matrix);
+
+  return 0;
+}
---
+++ trunk/c/affine2.c
@@ -0,0 +1,58 @@
+#include <cv.h>
+#include <highgui.h>
+
+int
+main(int argc, char **argv)
+{
+  double angle = -45.0, scale = 1.0;
+  IplImage *src_img = 0, *dst_img = 0;
+  CvMat *map_matrix;
+  CvPoint2D32f center;
+  CvPoint pt1, pt2;
+  CvRect rect;
+
+  // (1)画像の読み込み，出力用画像領域の確保を行なう
+  if(argc >= 2)
+    src_img = cvLoadImage(argv[1], CV_LOAD_IMAGE_ANYDEPTH | CV_LOAD_IMAGE_ANYCOLOR);
+  if(src_img == 0)
+    return -1;
+
+  rect.x = (int) (src_img->width * 0.25);
+  rect.y = (int) (src_img->height * 0.25);
+  rect.width = (int) (src_img->width * 0.5);
+  rect.height = (int) (src_img->height * 0.5);
+  cvSetImageROI(src_img, rect);
+
+  dst_img = cvCloneImage(src_img);
+
+  // (2)角度，スケール係数，回転中心を指定して
+  //    cv2DRotationMatrixを用いてアフィン行列を求める
+  map_matrix = cvCreateMat(2, 3, CV_32FC1);
+  center = cvPoint2D32f(src_img->width * 0.25, src_img->height * 0.25);
+  cv2DRotationMatrix(center, angle, scale, map_matrix);
+
+  // (3)指定されたアフィン行列により，cvWarpaffineを用いて画像のROI部分を回転させる
+  cvWarpAffine(src_img, dst_img, map_matrix, CV_INTER_LINEAR + CV_WARP_FILL_OUTLIERS, cvScalarAll(255));
+
+  // (4)結果を表示する
+  cvResetImageROI(src_img);
+  cvResetImageROI(dst_img);
+
+  pt1 = cvPoint(rect.x, rect.y);
+  pt2 = cvPoint(rect.x + rect.width, rect.y + rect.height);
+  cvRectangle(src_img, pt1, pt2, CV_RGB(255, 0, 255), 2, 8, 0);
+
+  cvNamedWindow("src", CV_WINDOW_AUTOSIZE);
+  cvNamedWindow("dst", CV_WINDOW_AUTOSIZE);
+  cvShowImage("src", src_img);
+  cvShowImage("dst", dst_img);
+  cvWaitKey(0);
+
+  cvDestroyWindow("src");
+  cvDestroyWindow("dst");
+  cvReleaseImage(&src_img);
+  cvReleaseImage(&dst_img);
+  cvReleaseMat(&map_matrix);
+
+  return 1;
+}

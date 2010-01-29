c5db204a757755d8652041e54c2aa6a86ea30fed
idojun <idojun@18163362-01b4-11df-b4af-25c34a55dae2>
2010-01-29 09:45:26 +0000

    add some images. add c/combine_images.c

---
+++ c/combine_images.c
@@ -0,0 +1,53 @@
+#include <cv.h>
+#include <highgui.h>
+
+int
+main (int argc, char **argv)
+{
+  int i, img_num;
+  int total_width=0, max_height=0;
+  IplImage **src_img;
+  IplImage *combined_img;
+  CvRect roi = cvRect(0, 0, 0, 0);
+
+  // (1)コマンド引数で指定された画像を全て読み込む
+  if(argc < 2) {
+    return 1;
+  } else {
+    img_num = argc - 1;
+    src_img = (IplImage**)cvAlloc(sizeof(IplImage*)*img_num);
+    for(i=0; i<img_num; i++) {
+      src_img[i] = cvLoadImage (argv[i+1], CV_LOAD_IMAGE_COLOR);
+      if(src_img[i] == 0)
+	return -1;
+      total_width += src_img[i]->width;
+      max_height = max_height < src_img[i]->height ? src_img[i]->height : max_height;
+    }
+  }
+
+  // (2)画像を横方向に連結する
+  combined_img = cvCreateImage(cvSize(total_width, max_height), IPL_DEPTH_8U, 3);
+  cvZero(combined_img);
+  for(i=0; i<img_num; i++) {
+    roi.width = src_img[i]->width;
+    roi.height = src_img[i]->height;
+    cvSetImageROI(combined_img, roi);
+    cvCopy(src_img[i], combined_img, NULL);
+    roi.x += roi.width;
+  }
+  cvResetImageROI(combined_img);
+
+  // 
+  cvNamedWindow ("Image", CV_WINDOW_AUTOSIZE);
+  cvShowImage ("Image", combined_img);
+  cvWaitKey (0);
+
+  cvDestroyWindow("Image");
+  cvReleaseImage(&combined_img);
+  for(i=0; i<img_num; i++) {
+    cvReleaseImage(&src_img[i]);
+  }
+  cvFree(&src_img);
+
+  return 0;
+}

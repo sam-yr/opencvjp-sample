0f868c3126c07bf35aadaba4f924c5ad0640d7a0
idojun <idojun@18163362-01b4-11df-b4af-25c34a55dae2>
2010-02-05 15:53:33 +0000

    modify cpp/color-sub_cpp.cpp (to work well).

--- cpp/color-sub_cpp.cpp
+++ cpp/color-sub_cpp.cpp
@@ -13,23 +13,31 @@ main (int argc, char **argv)
   if(!src_img.data)
     return -1;
 
-  //Mat points(Size(src_img.rows*src_img.cols, 1), CV_32FC3);
-  Mat points = src_img.reshape(3, 1);
-  std::cout << points.channels() << "ch ," << points.rows << "," << points.cols << ":" << points.isContinuous() << std::endl;
+  Mat points;
   const int cluster_count = 32;
-  //Mat points;
-  points.convertTo(points, CV_32FC3);
-  Mat clusters(points.size(), CV_32SC1);
-  std::cout << clusters.channels() << "ch ," << clusters.rows << "," << clusters.cols << ":" << clusters.isContinuous() << std::endl;
-  cvKMeans2(&IplImage(points), cluster_count, &IplImage(clusters), cvTermCriteria( CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 10, 1.0 ));
-  
+  src_img.convertTo(points, CV_32FC3);
+  points = points.reshape(3, src_img.rows*src_img.cols);
 
-  std::cout << clusters.channels() << "," << clusters.rows << "," << clusters.cols << std::endl;
-  //  for(int i=0; i<cluster_count; ++i)
-  //    std::cout << clusters.at<int>(i,1) << std::endl;
+  Mat_<int> clusters(points.size(), CV_32SC1);
+  Mat centers;
+  kmeans(points, cluster_count, clusters, 
+	 cvTermCriteria(CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 10, 1.0), 1, KMEANS_PP_CENTERS, &centers);
+  
+  MatIterator_<Vec3f> itf = centers.begin<Vec3f>();
+  Mat dst_img(src_img.size(), src_img.type());
+  uchar *p = dst_img.ptr<uchar>();
+  int inc = dst_img.elemSize()/sizeof(uchar);
+  for(int i=0; i<dst_img.cols*dst_img.rows; i++, p+=inc) {
+    p[0] = saturate_cast<uchar>(itf[clusters(1,i)][0]);
+    p[1] = saturate_cast<uchar>(itf[clusters(1,i)][1]);
+    p[2] = saturate_cast<uchar>(itf[clusters(1,i)][2]);
+  }
+
   
   namedWindow("Image", CV_WINDOW_AUTOSIZE);
   imshow("Image", src_img);
+  namedWindow("dst_img", CV_WINDOW_AUTOSIZE);
+  imshow("dst_img", dst_img);
   waitKey(0);
 
   return 0;

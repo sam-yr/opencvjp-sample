7bbac8cc80bba46bba26700e75efc1f52ef97eb6
idojun <idojun@18163362-01b4-11df-b4af-25c34a55dae2>
2010-02-23 12:59:25 +0000

    modify affine2.c to add some comments add some images (under CC license) modify CMakeLists.txt to be compiled with openMP

--- trunk/c/affine2.c
+++ trunk/c/affine2.c
@@ -18,26 +18,28 @@ main(int argc, char **argv)
   src_img = cvLoadImage(imagename, CV_LOAD_IMAGE_COLOR);
   if(src_img == 0)
     return -1;
+  dst_img = cvCloneImage(src_img);
+
+  // (2)set ROI
   rect.x = cvRound(src_img->width * 0.25);
   rect.y = cvRound(src_img->height * 0.25);
   rect.width = cvRound(src_img->width * 0.5);
   rect.height = cvRound(src_img->height * 0.5);
   cvSetImageROI(src_img, rect);
-  dst_img = cvCloneImage(src_img);
 
-  // (2)With specified three parameters (angle, rotation center, scale)
+
+  // (3)with specified three parameters (angle, rotation center, scale)
   //    calculate an affine transformation matrix by cv2DRotationMatrix
   affine_matrix = cvCreateMat(2, 3, CV_32FC1);
   center = cvPoint2D32f(src_img->width * 0.25, src_img->height * 0.25);
   cv2DRotationMatrix(center, angle, scale, affine_matrix);
 
-  // (3)rotate the image by warpAffine taking the affine matrix
+  // (4)rotate the image by warpAffine taking the affine matrix
   cvWarpAffine(src_img, dst_img, affine_matrix, CV_INTER_LINEAR + CV_WARP_FILL_OUTLIERS, cvScalarAll(255));
 
-  // (4)reset ROI, then show source and destination image with a rectangle indicating ROI
+  // (5)reset ROI, then show source and destination image with a rectangle indicating ROI
   cvResetImageROI(src_img);
   cvResetImageROI(dst_img);
-
   pt1 = cvPoint(rect.x, rect.y);
   pt2 = cvPoint(rect.x + rect.width, rect.y + rect.height);
   cvRectangle(src_img, pt1, pt2, CV_RGB(255, 0, 255), 2, 8, 0);

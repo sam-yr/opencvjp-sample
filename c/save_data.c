252916a3e5750dfef29c4ea7f3e0e7d18accd121
idojun <idojun@18163362-01b4-11df-b4af-25c34a55dae2>
2010-02-24 15:17:52 +0000

    add cpp/feature_detect_cpp.cpp

--- c/save_data.c
+++ c/save_data.c
@@ -3,43 +3,43 @@
 int
 main (int argc, char **argv)
 {
-	char filename[] = "save_cv.xml";	// file name
-	int i;
-	int a;
-	float b;
-	CvMat** mat;
-	CvFileStorage *cvfs;
+  char filename[] = "save_cv.xml";	// file name
+  int i;
+  int a;
+  float b;
+  CvMat** mat;
+  CvFileStorage *cvfs;
 
-	// (1)create sample data
-	a = 10;
-	b = 0.1;
-	mat = (CvMat**)malloc(3*sizeof(CvMat*));
-	for(i=0;i<3;i++){
-		mat[i] = cvCreateMat(3,3,CV_32FC1);
-		cvSet(mat[i], cvScalar(i));
-	}
+  // (1)create sample data
+  a = 10;
+  b = 0.1;
+  mat = (CvMat**)cvAlloc(3*sizeof(CvMat*));
+  for(i=0;i<3;i++){
+    mat[i] = cvCreateMat(3,3,CV_32FC1);
+    cvSet(mat[i], cvScalarAll(i), NULL);
+  }
 
-	// (2)open file storage
-	cvfs = cvOpenFileStorage(filename,NULL,CV_STORAGE_WRITE);
+  // (2)open file storage
+  cvfs = cvOpenFileStorage(filename,NULL,CV_STORAGE_WRITE);
 
-	// (3)write data to file
-	cvWriteInt(cvfs,"a",a);
-	cvWriteReal(cvfs,"b",b);
+  // (3)write data to file
+  cvWriteInt(cvfs, "a", a);
+  cvWriteReal(cvfs, "b", b);
 
-	cvStartWriteStruct(cvfs, "mat_array", CV_NODE_SEQ);		// create node
-	for(i=0; i<3; i++){
-		cvWrite(cvfs,NULL,mat[i]);
-	}
-	cvEndWriteStruct(cvfs);
+  cvStartWriteStruct(cvfs, "mat_array", CV_NODE_SEQ, NULL, cvAttrList(NULL, NULL));	// create node
+  for(i=0; i<3; i++){
+    cvWrite(cvfs, NULL, mat[i], cvAttrList(NULL, NULL));
+  }
+  cvEndWriteStruct(cvfs);
 
-	// (4)close file storage
-	cvReleaseFileStorage(&cvfs);
+  // (4)close file storage
+  cvReleaseFileStorage(&cvfs);
 
-	// release mat
-	for(i=0; i<3; i++){
-		cvReleaseMat(mat+i);
-	}
-	free(mat);
+  // release mat
+  for(i=0; i<3; i++){
+    cvReleaseMat(mat+i);
+  }
+  cvFree(mat);
 
-	return 0;
+  return 0;
 }
--- cpp/load_data_cpp.cpp
+++ cpp/load_data_cpp.cpp
@@ -2,45 +2,44 @@
 #include <cv.h>
 
 using namespace std;
-using namespace cv;
 
 int
 main (int argc, char **argv)
 {
-	char filename[] = "save_cv.xml";	// file name
-	int i,j,k;
+  char filename[] = "save_cv.xml";	// file name
+  int i,j,k;
 
-	// (1)memory space for loading data
-	int a;
-	float b;
-	cv::Mat mat[3];
+  // (1)memory space for loading data
+  int a;
+  float b;
+  cv::Mat mat[3];
 
-	// (2)open file storage
-	cv::FileStorage	cvfs(filename,CV_STORAGE_READ);
+  // (2)open file storage
+  cv::FileStorage cvfs(filename,CV_STORAGE_READ);
 
-	// (3)read data from file storage
-	cv::FileNode node(cvfs.fs, NULL);	// Get Top Node
-	a = node["a"];
-	b = node["b"];
-	cv::FileNode fn = node["mat_array"];
+  // (3)read data from file storage
+  cv::FileNode node(cvfs.fs, NULL);	// Get Top Node
+  a = node["a"];
+  b = node["b"];
+  cv::FileNode fn = node["mat_array"];
 	
-	for(i=0;i<fn.size();i++){
-		cv::read(fn[i], mat[i]);
-	}
+  for(i=0;i<fn.size();i++){
+    cv::read(fn[i], mat[i]);
+  }
 
-	// (4)print loaded data
-	cout << "a:" << a << endl;
-	cout << "b:" << b << endl;
+  // (4)print loaded data
+  cout << "a:" << a << endl;
+  cout << "b:" << b << endl;
 
-	for(i=0; i<3; i++){
-		cout << "mat" << i << ":" << endl;
-		for(j=0;j<mat[i].rows;j++){
-			for(k=0;k<mat[i].cols;k++){
-				cout << mat[i].at<float>(j,k) << ",";
-			}
-			cout << endl;
-		}
-	}
+  for(i=0; i<3; i++){
+    cout << "mat" << i << ":" << endl;
+    for(j=0;j<mat[i].rows;j++){
+      for(k=0;k<mat[i].cols;k++){
+	cout << mat[i].at<float>(j,k) << ",";
+      }
+      cout << endl;
+    }
+  }
 
-	return 0;
+  return 0;
 }

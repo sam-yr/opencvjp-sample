d99bf5732620ad4c9377341e1c567248dcba8205
Norishige.Fukushima <Norishige.Fukushima@18163362-01b4-11df-b4af-25c34a55dae2>
2011-03-01 11:12:43 +0000

    point_cloud_test.cpp bug fixed

--- trunk/cpp/point_cloud_test.cpp
+++ trunk/cpp/point_cloud_test.cpp
@@ -1,12 +1,11 @@
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
-
 void reprojectKinectDepth3D(Mat& src, Mat& dest, const double focal_length, Point2d imageCenter=Point2d(-1,-1))
 {
 	if(dest.empty())dest=Mat::zeros(src.size().area(),1,CV_32FC3);
-	
-	 const double bigZ = 10000.;
+
+	const double bigZ = 10000.;
 	const double hw=(src.cols-1)*0.5;
 	const double hh=(src.rows-1)*0.5;
 	if(imageCenter.x==-1&&imageCenter.y==-1)imageCenter=Point2d(hw,hh);
@@ -22,163 +21,162 @@ void reprojectKinectDepth3D(Mat& src, Mat& dest, const double focal_length, Poin
 			data[1]=*s*ifocal_length*(j-imageCenter.y);
 			if(*s==0)data[2]=bigZ;
 			else data[2]=*s;
-			
+
 			data+=3;
 			s++;
 		}
 	}
 }
 
-	template <class T>
-	static void fillOcclusionInv_(Mat& src, T invalidvalue)
-	{
-		int bb=1;
-		const int MAX_LENGTH=src.cols*0.8;
+template <class T>
+static void fillOcclusionInv_(Mat& src, T invalidvalue)
+{
+	int bb=1;
+	const int MAX_LENGTH=src.cols*0.8;
 #pragma omp parallel for
-		for(int j=bb;j<src.rows-bb;j++)
+	for(int j=bb;j<src.rows-bb;j++)
+	{
+		T* s = src.ptr<T>(j);
+		const T st = s[0];
+		const T ed = s[src.cols-1];
+		s[0]=0;
+		s[src.cols-1]=0;
+		for(int i=0;i<src.cols;i++)
 		{
-			T* s = src.ptr<T>(j);
-			const T st = s[0];
-			const T ed = s[src.cols-1];
-			s[0]=0;
-			s[src.cols-1]=0;
-			for(int i=0;i<src.cols;i++)
+			if(s[i]==invalidvalue)
 			{
-				if(s[i]==invalidvalue)
+				int t=i;
+				do
 				{
-					int t=i;
-					do
-					{
-						t++;
-						if(t>src.cols-1)break;
-					}while(s[t]==invalidvalue);
+					t++;
+					if(t>src.cols-1)break;
+				}while(s[t]==invalidvalue);
 
-					const T dd = max(s[i-1],s[t]);
-					if(t-i>MAX_LENGTH)
+				const T dd = max(s[i-1],s[t]);
+				if(t-i>MAX_LENGTH)
+				{
+					for(int n=0;n<src.cols;n++)
 					{
-						for(int n=0;n<src.cols;n++)
-						{
-							s[n]=invalidvalue;
-						}
+						s[n]=invalidvalue;
 					}
-					else
+				}
+				else
+				{
+					for(;i<t;i++)
 					{
-						for(;i<t;i++)
-						{
-							s[i]=dd;
-						}
+						s[i]=dd;
 					}
 				}
 			}
 		}
 	}
+}
 
-	template <class T>
-	static void fillOcclusion_(Mat& src, T invalidvalue)
-	{
-		int bb=1;
-		const int MAX_LENGTH=src.cols*0.5;
+template <class T>
+static void fillOcclusion_(Mat& src, T invalidvalue)
+{
+	int bb=1;
+	const int MAX_LENGTH=src.cols*0.5;
 #pragma omp parallel for
-		for(int j=bb;j<src.rows-bb;j++)
+	for(int j=bb;j<src.rows-bb;j++)
+	{
+		T* s = src.ptr<T>(j);
+		const T st = s[0];
+		const T ed = s[src.cols-1];
+		s[0]=255;
+		s[src.cols-1]=255;
+		for(int i=0;i<src.cols;i++)
 		{
-			T* s = src.ptr<T>(j);
-			const T st = s[0];
-			const T ed = s[src.cols-1];
-			s[0]=255;
-			s[src.cols-1]=255;
-			for(int i=0;i<src.cols;i++)
+			if(s[i]<=invalidvalue)
 			{
-				if(s[i]<=invalidvalue)
+				int t=i;
+				do
 				{
-					int t=i;
-					do
-					{
-						t++;
-						if(t>src.cols-1)break;
-					}while(s[t]<=invalidvalue);
+					t++;
+					if(t>src.cols-1)break;
+				}while(s[t]<=invalidvalue);
 
-					const T dd = min(s[i-1],s[t]);
-					if(t-i>MAX_LENGTH)
+				const T dd = min(s[i-1],s[t]);
+				if(t-i>MAX_LENGTH)
+				{
+					for(int n=0;n<src.cols;n++)
 					{
-						for(int n=0;n<src.cols;n++)
-						{
-							s[n]=invalidvalue;
-						}
+						s[n]=invalidvalue;
 					}
-					else
+				}
+				else
+				{
+					for(;i<t;i++)
 					{
-						for(;i<t;i++)
-						{
-							s[i]=dd;
-						}
+						s[i]=dd;
 					}
 				}
 			}
 		}
 	}
-	void fillOcclusion(Mat& src, int invalidvalue, bool isInv=false)
+}
+void fillOcclusion(Mat& src, int invalidvalue, bool isInv=false)
+{
+	if(isInv)
 	{
-		if(isInv)
+		if(src.type()==CV_8U)
 		{
-			if(src.type()==CV_8U)
-			{
-				fillOcclusionInv_<uchar>(src, (uchar)invalidvalue);
-			}
-			else if(src.type()==CV_16S)
-			{
-				fillOcclusionInv_<short>(src, (short)invalidvalue);
-			}
-			else if(src.type()==CV_16U)
-			{
-				fillOcclusionInv_<unsigned short>(src, (unsigned short)invalidvalue);
-			}
-			else if(src.type()==CV_32F)
-			{
-				fillOcclusionInv_<float>(src, (float)invalidvalue);
-			}
-
+			fillOcclusionInv_<uchar>(src, (uchar)invalidvalue);
 		}
-		else
+		else if(src.type()==CV_16S)
 		{
-			if(src.type()==CV_8U)
-			{
-				fillOcclusion_<uchar>(src, (uchar)invalidvalue);
-			}
-			else if(src.type()==CV_16S)
-			{
-				fillOcclusion_<short>(src, (short)invalidvalue);
-			}
-			else if(src.type()==CV_16U)
-			{
-				fillOcclusion_<unsigned short>(src, (unsigned short)invalidvalue);
-			}
-			else if(src.type()==CV_32F)
-			{
-				fillOcclusion_<float>(src, (float)invalidvalue);
-			}
+			fillOcclusionInv_<short>(src, (short)invalidvalue);
+		}
+		else if(src.type()==CV_16U)
+		{
+			fillOcclusionInv_<unsigned short>(src, (unsigned short)invalidvalue);
+		}
+		else if(src.type()==CV_32F)
+		{
+			fillOcclusionInv_<float>(src, (float)invalidvalue);
 		}
 	}
-	void eular2rot(double yaw,double pitch, double roll,Mat& dest)
+	else
 	{
-		double theta = yaw/180.0*CV_PI;
-		double pusai = pitch/180.0*CV_PI;
-		double phi = roll/180.0*CV_PI;
-
-		double datax[3][3] = {{1.0,0.0,0.0}, 
-		{0.0,cos(theta),-sin(theta)}, 
-		{0.0,sin(theta),cos(theta)}};
-		double datay[3][3] = {{cos(pusai),0.0,sin(pusai)}, 
-		{0.0,1.0,0.0}, 
-		{-sin(pusai),0.0,cos(pusai)}};
-		double dataz[3][3] = {{cos(phi),-sin(phi),0.0}, 
-		{sin(phi),cos(phi),0.0}, 
-		{0.0,0.0,1.0}};
-		Mat Rx(3,3,CV_64F,datax);
-		Mat Ry(3,3,CV_64F,datay);
-		Mat Rz(3,3,CV_64F,dataz);
-		Mat rr=Rz*Rx*Ry;
-		rr.copyTo(dest);
+		if(src.type()==CV_8U)
+		{
+			fillOcclusion_<uchar>(src, (uchar)invalidvalue);
+		}
+		else if(src.type()==CV_16S)
+		{
+			fillOcclusion_<short>(src, (short)invalidvalue);
+		}
+		else if(src.type()==CV_16U)
+		{
+			fillOcclusion_<unsigned short>(src, (unsigned short)invalidvalue);
+		}
+		else if(src.type()==CV_32F)
+		{
+			fillOcclusion_<float>(src, (float)invalidvalue);
+		}
 	}
+}
+void eular2rot(double yaw,double pitch, double roll,Mat& dest)
+{
+	double theta = yaw/180.0*CV_PI;
+	double pusai = pitch/180.0*CV_PI;
+	double phi = roll/180.0*CV_PI;
+
+	double datax[3][3] = {{1.0,0.0,0.0}, 
+	{0.0,cos(theta),-sin(theta)}, 
+	{0.0,sin(theta),cos(theta)}};
+	double datay[3][3] = {{cos(pusai),0.0,sin(pusai)}, 
+	{0.0,1.0,0.0}, 
+	{-sin(pusai),0.0,cos(pusai)}};
+	double dataz[3][3] = {{cos(phi),-sin(phi),0.0}, 
+	{sin(phi),cos(phi),0.0}, 
+	{0.0,0.0,1.0}};
+	Mat Rx(3,3,CV_64F,datax);
+	Mat Ry(3,3,CV_64F,datay);
+	Mat Rz(3,3,CV_64F,dataz);
+	Mat rr=Rz*Rx*Ry;
+	rr.copyTo(dest);
+}
 
 void lookat(Point3d from, Point3d to, Mat& destR)
 {
@@ -212,8 +210,8 @@ static void projectImagefromXYZ_(Mat& image, Mat& destimage, Mat& disp, Mat& des
 		uchar* m=mask.ptr<uchar>(j);
 		for(int i=0;i<image.cols;i++,count++)
 		{
-			int x=(int)pt[count].x;
-			int y=(int)pt[count].y;
+			int x=(int)(pt[count].x+0.5);
+			int y=(int)(pt[count].y+0.5);
 			if(m[i]==255)continue;
 			if(pt[count].x>=1 && pt[count].x<image.cols-1 && pt[count].y>=1 && pt[count].y<image.rows-1)
 			{
@@ -268,7 +266,7 @@ static void projectImagefromXYZ_(Mat& image, Mat& destimage, Mat& disp, Mat& des
 							destimage.at<uchar>(y,3*x+3)=img[3*i+0];
 							destimage.at<uchar>(y,3*x+4)=img[3*i+1];
 							destimage.at<uchar>(y,3*x+5)=img[3*i+2];
-							
+
 							destdisp.at<T>(y,x+1)=disp.at<T>(j,i);
 						}
 						else if((int)pt[count-1].x-x<-1)
@@ -276,7 +274,7 @@ static void projectImagefromXYZ_(Mat& image, Mat& destimage, Mat& disp, Mat& des
 							destimage.at<uchar>(y,3*x-3)=img[3*i+0];
 							destimage.at<uchar>(y,3*x-2)=img[3*i+1];
 							destimage.at<uchar>(y,3*x-1)=img[3*i+2];
-							
+
 							destdisp.at<T>(y,x-1)=disp.at<T>(j,i);
 						}
 						else if((int)pt[count+image.cols].y-y>1)
@@ -397,7 +395,7 @@ void stereoTest()
 	Mat image=imread("l.png",1);
 	Mat imageR=imread("r.png",1);
 	Mat destimage;
-	
+
 	StereoSGBM sgbm(1,16*2,3,200,255,1,0,0,0,0,true);
 	Mat disp;
 	Mat destdisp;
@@ -421,7 +419,7 @@ void stereoTest()
 	K.at<double>(0,2)=(image.cols-1.0)/2.0;
 	K.at<double>(1,2)=(image.rows-1.0)/2.0;
 
-	Mat dist=Mat::zeros(8,1,CV_64F);
+	Mat dist=Mat::zeros(5,1,CV_64F);
 	Mat R=Mat::eye(3,3,CV_64F);
 	Mat t=Mat::zeros(3,1,CV_64F);
 
@@ -430,17 +428,18 @@ void stereoTest()
 	const double step=baseline;
 	int key=0;
 	bool isSub=true;
+	//(4) rendering loop
 	while(key!='q')
 	{
 		lookat(viewpoint, lookatpoint , R);
 		t.at<double>(0,0)=viewpoint.x;
 		t.at<double>(1,0)=viewpoint.y;
 		t.at<double>(2,0)=viewpoint.z;
-		
+
 		cout<<t<<endl;
 		t=R*t;
 
-		//(4) projecting 3D point cloud to image.
+		//(5) projecting 3D point cloud to image.
 		projectImagefromXYZ(image,destimage,disp,destdisp,xyz,R,t,K,dist,isSub);
 
 		destdisp.convertTo(dispshow,CV_8U,0.5);
@@ -448,7 +447,6 @@ void stereoTest()
 		imshow("image",destimage);
 
 		key = waitKey(1);
-          	//(5) key map
 		if(key=='f')
 		{
 			isSub=isSub?false:true;
@@ -493,7 +491,7 @@ void kinectTest()
 	compare(depth16,0,mask,CMP_EQ);
 
 	Mat imni=imread("kinectimage.png");
-	
+
 	//virtual camera setting
 	const double focal_length = 580.0;
 	const double baseline = 75;
@@ -511,8 +509,6 @@ void kinectTest()
 	Point3d lookatpoint(0.0,0.0,-baseline*10.0);
 	const double step=30.0;
 
-	
-	
 	//(2) make Q matrix and reproject pixels into 3D space
 	//project depth map to xyz 3D space
 	Mat Q=makeQMatrix(Point2d((640-1.0)/2.0,(480-1.0)/2.0),focal_length,baseline*16);
@@ -541,7 +537,7 @@ void kinectTest()
 		t.at<double>(1,0)=viewpoint.y;
 		t.at<double>(2,0)=viewpoint.z;
 		t=R*t;
-		
+
 		projectImagefromXYZ(imni,destim,depthshow,destdisp,xyz,R,t,K,dist,isSub,mask);
 
 		imshow("warp depth",destdisp);
@@ -586,6 +582,6 @@ int main(int argc, char** argv)
 	stereoTest();
 	//(B)Sample of Kinect 3D point cloud rendering
 	kinectTest();
-	
+
 	return 0;
 }
